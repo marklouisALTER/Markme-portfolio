3f6d2354860834e3ecc46de5f44cb5b3e7758b1d
Mark Louis Bernardo <marklouisbernando318@gmail.com>
2024-07-28 21:12:11 +0800

    fix: bug in api folder

--- frontend/src/api/axiosInstance.tsx
+++ frontend/src/api/axiosInstance.tsx
@@ -1,14 +1,14 @@
-import axios, { AxiosRequestConfig } from 'axios'
+// import axios, { AxiosRequestConfig } from 'axios'
 
-const axiosInstance = axios.create({
-    url: 'http://localhost:3001/',
-    withCredentials: true,
-})
+// const axiosInstance = axios.create({
+//     url: 'http://localhost:3001/',
+//     withCredentials: true,
+// })
 
-axiosInstance.interceptors.request.use(
-    async (config: AxiosRequestConfig) => {
-        // Add your custom headers here
-        config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`
-        return config
-    }
-)
+// axiosInstance.interceptors.request.use(
+//     async (config: AxiosRequestConfig) => {
+//         // Add your custom headers here
+//         config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+//         return config
+//     }
+// )
